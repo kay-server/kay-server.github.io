9c64c6c8e80d1310b5bf36fe7cba592584e11223
kay-server <kaykokh@internet.ru>
2022-02-15 18:13:57 +0300

    -

--- download/panel.js
+++ download/panel.js
@@ -1,4 +1,4 @@
-const copyrights = '<h3>Powered by <a href="https://kay-software.ru/" target="_blank">Kay Software</a> company</h3><a href="../download-panel" target="_blank" style="font-size: 14px; color: gray; text-decoration: underline;">USE THIS FILE EXCHANGER WITHOUT LIMITS FOR FREE</a>';const paramsList = new URLSearchParams(document.location.search);const cloud = "https://raw.githubusercontent.com/";const defaultAccount = "kay-server";const verification_code_source = '<img src="verified.png" width="16" height="16" style="cursor: pointer; margin: -2.5px;" title="Verified"><br><span style="color: gray;">The file is checked for viruses.</span>';const dev_code_source = '<img src="verified.png" width="16" height="16" style="cursor: pointer; margin: -2.5px;" title="Verified (service account)"><br><span style="color: gray; font-size: 14px;">The files were uploaded by a service account and<br>do not contain malicious code.</span>';
+const copyrights = '<h3>Powered by <a href="https://kay-software.ru/" target="_blank">Kay Software</a> company</h3><a href="../download-panel" target="_blank" style="font-size: 14px; color: gray;"><br><img src="banner.png" height="120" width="270"></a>';const paramsList = new URLSearchParams(document.location.search);const cloud = "https://raw.githubusercontent.com/";const defaultAccount = "kay-server";const verification_code_source = '<img src="verified.png" width="16" height="16" style="cursor: pointer; margin: -2.5px;" title="Verified"><br><span style="color: gray;">The file is checked for viruses.</span>';const dev_code_source = '<img src="verified.png" width="16" height="16" style="cursor: pointer; margin: -2.5px;" title="Verified (service account)"><br><span style="color: gray; font-size: 14px;">The files were uploaded by a service account and<br>do not contain malicious code.</span>';
 let currentFileName = paramsList.get('file');
 let account = paramsList.get('from');
 switch (account) {case "":account = defaultAccount;break;case null:account = defaultAccount;break;}
@@ -13,7 +13,8 @@ switch (account) {
 let DownloadWorkerZone = document.getElementById('get-file');
 function req_err() {
 	console.log("Request is empty.");
-	DownloadWorkerZone.innerHTML = '<br><h2>Kay Download Panel</h2><hr><h3 style="color: white;">Use file hosting without restrictions for free. Read how it works and how you can upload your files here.<br><br><button onclick="opensite()" class="kay-download-panel-menu-button" style="font-size: 25px;">Read now</button><br><br></h3><hr>' + copyrights;
+	document.title = 'Kay Download Panel';
+	DownloadWorkerZone.innerHTML = '<br><h2>Kay Download Panel</h2><hr><h3 style="color: white;">Use file hosting without limits for free. Read how it works and how you can upload your files here.<br><br><button onclick="opensite()" class="kay-download-panel-menu-button" style="font-size: 25px;">Read now</button><br><br></h3><hr>' + copyrights;
 }
 switch (currentFileName) {
 	case "":req_err();break;case null:req_err();break;default:
@@ -24,9 +25,9 @@ switch (currentFileName) {
 				try {
 					DownloadWorkerZone.innerHTML = '<meta http-equiv="refresh" content="0;URL=' + cloud + account + '/kay-software-cloud/main/' + currentFileName + '"><br><h2>Download "<span style="color: #fffdc2;">' + currentFileName + '</span>" started automatically!</h2><hr>' + '<h3 style="color: white;"><b>Thanks! You can close this tab.</b><br>If the download does not start automatically, try changing your browser or contact support. In this case, also check the possibility of downloading files from the Internet.</h3><hr>' + copyrights;
 					console.log("Seccuss!");
-					} catch (e) {
-						console.log("Error: " + e + " [Download]");
-					}
+				} catch (e) {
+					console.log("Error: " + e + " [Download]");
+				}
 			}
 		} catch (e) {
 			console.log("Error: " + e + " [Main]")
