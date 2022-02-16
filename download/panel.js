703e9c44300a63c3e2cba8ba52073050a4111f95
kay-server <kaykokh@internet.ru>
2022-02-16 17:47:53 +0300

    -

--- download/panel.js
+++ download/panel.js
@@ -1,4 +1,4 @@
-const panel = "https://kay-software.ru/download/"
+const panel = window.location.hostname + "/download/"
 const copyrights = '<h3>Powered by <a href="https://kay-software.ru/" target="_blank">Kay Software</a> company</h3><a href="../download-panel" target="_blank" style="font-size: 14px; color: gray;"><br><img src="banner.png" height="120" width="270"></a>';
 const verification_code_source = '<img src="verified.png" width="16" height="16" style="cursor: pointer; margin: -2.5px;" title="Verified"><br><span style="color: gray;">The file is checked for viruses.</span>';const dev_code_source = '<img src="verified.png" width="16" height="16" style="cursor: pointer; margin: -2.5px;" title="Verified (service account)"><br><span style="color: gray; font-size: 14px;">The files were uploaded by a service account and<br>do not contain malicious code.</span>';
 const paramsList = new URLSearchParams(document.location.search);
