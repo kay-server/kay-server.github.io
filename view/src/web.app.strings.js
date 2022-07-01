c82f0f26194645fb1e537fdeb2384e6cfb846771
winlocker-site <kaykokh@internet.ru>
2022-07-01 20:51:21 +0300

    -

--- view/src/web.app.strings.js
+++ view/src/web.app.strings.js
@@ -1,3 +1,4 @@
 const _OnLoadError = "<center><h3>Не удалось загрузить статью. Обратитесь к разработчикам сервиса или повторите попытку позже.</h3></center>";
-const _OnLoadErrorT = "Ошибка... - "
+const _OnLoadErrorT = "Ошибка... - ";
 const _PublicationDate = "<p style = \"display: inline-block; font-size: 12px; text-shadow: none; padding: 3px 16px; margin-bottom: 25px;color: #313131; background: #10ddb4;\" ><b>{%s}</b></p>";
+const _LoadingLabel = "Загрузка контента...";
