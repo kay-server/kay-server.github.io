095da84038e4fda2211a5c1e3741faabffc325fa
winlocker-site <kaykokh@internet.ru>
2022-07-25 00:08:25 +0300

    -

--- neuro/_data1.js
+++
@@ -1,3 +0,0 @@
-if (document.domain !== "kay" + "-" + "software" + ".ru") {location = "/"}
-function PsRand(a, b) {return a + Math.floor(Math.random() * b);}
-fetch("seed/" + PsRand(0, 6220) + ".txt").then(response => response.text()).then(code => document.write(code));
