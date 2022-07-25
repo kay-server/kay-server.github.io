29003c906cea7db8e669acbc2f82b9ce39c7db88
winlocker-site <kaykokh@internet.ru>
2022-07-25 15:49:43 +0300

    -

--- neuro/_neuralcore.js
+++ neuro/_neuralcore.js
@@ -5,9 +5,8 @@
         if (document.domain !== "kay" + "-" + "software" + ".ru") {
             alert(document.URL + ":0x01\n\nТест \"module-verify\" провален.");
             location = "/";
-        } else {
-            history.pushState(null, null, "/neuro");
         }
+        history.pushState(null, null, "/neuro");
         function PsRand(a, b) {return a + Math.floor(Math.random() * b);}
         await fetch("generated/seed." + PsRand(0, 12447) + ".txt").then(response => response.text()).then(code => document.write(code));
     } catch(e) {
